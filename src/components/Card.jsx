198d607ea478c2c4763935ede8e82d0f92102a23
Matthew Clendenning <124844665+Matthew-Clendenning@users.noreply.github.com>
2024-10-16 20:26:13 -0500

    Removed style sheets that could be implemented as inline styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import '../styles/Card.css';
-
 const Card = ({ children }) => {
     return (
-        <div className="card">
+        <div 
+            className="card"
+            style={{ 
+                display: 'grid',
+                justifyContent: 'center',
+                justifyItems: 'center',
+                width: '300px',
+                paddingBlock: '1rem',
+                border: '1px solid #ddd',
+                borderRadius: '10px',
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                backgroundColor: '#fff'
+            }}>
             {children}
         </div>
     );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import '../styles/Footer.css';
-
 const Footer = () => {
   return (
     <>
       <footer>
-        <p>A Survey on the QUIC Protocol</p>
+        <p style={{ 
+          fontSize: '18px',
+          textAlign: 'center',
+          color: 'white',
+          background: '#516059',
+          padding: '30px'
+        }}>A Survey on the QUIC Protocol</p>
       </footer>
     </>
   )
